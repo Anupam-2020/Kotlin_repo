d18f135a1ae69267aaa4e74703e0ef26a376dda2
Anupam-2020 <anupamanand.696@gmail.com>
2023-10-28 23:21:51 +0530

    added basic OOPs concept

---
+++ Classes/src/main/kotlin/Car.kt
@@ -0,0 +1,18 @@
+class Car(name: String, model: String, var color: String, var door: Int) {
+    private var model = model
+    var names = name
+    var lastName: String
+
+    init { // initializer gets called on every object creation.
+        if(name.lowercase().startsWith('a')) {
+            lastName = name
+        } else {
+            lastName = "User"
+            println("User doesn't start with A or a")
+        }
+    }
+
+    fun move() {
+        println("This car $names is moving and is of ${this.model} model modal with $door doors and $color color")
+    }
+}
---
+++ Classes/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val car1 = Car("BMW", "X7", "Blue", 4)
+//    car1.names = "BMWS"
+//    car1.modal = "X7"
+//    car1.color = "Blue"
+//    car1.door = 4
+
+    println(car1.names)
+    car1.move()
+}
